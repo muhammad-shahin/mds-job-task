7fe5c0acb17ff34258bc52201a4eb4958538aa78
muhammad-shahin <muhammadshahin002@gmail.com>
2024-01-16 21:17:02 +0600

    popup button card onclick function added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // alert('Javascript is connected!');
+
+const popupCard = document.getElementById('popupCard');
+const ellipsesBtn = document.getElementById('ellipses-btn');
+ellipsesBtn.addEventListener('click', function () {
+  const containsPopupClass = popupCard.classList.contains('popup');
+  if (containsPopupClass) {
+    popupCard.classList.remove('popup');
+  } else {
+    popupCard.classList.add('popup');
+  }
+});
