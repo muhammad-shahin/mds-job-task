c342c91512b3456625c77783c1a1e6ed36db3ccf
muhammad-shahin <muhammadshahin002@gmail.com>
2024-01-17 13:40:35 +0600

    data label change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // import { getPreviousData, addNewData } from './localDB';
 
-// toggle popup
+// mark as done & delete popup
 const openPopup = (event, BtnId) => {
   console.log(BtnId);
   const popupCard = document.getElementById('popupCard-' + BtnId);
@@ -47,6 +47,40 @@ const addNewData = (newData) => {
   localStorage.setItem('activitiesData', updatedData);
 };
 
+// delete data from localstorage
+const deleteData = (index) => {
+  let storedData = getPreviousData();
+  // Check if the index is valid
+  if (index >= 0 && index < storedData.length) {
+    // Use splice to remove the element at the specified index
+    storedData.splice(index, 1);
+  } else {
+    console.log('Invalid index');
+  }
+  const updatedData = JSON.stringify(storedData);
+  localStorage.setItem('activitiesData', updatedData);
+  showData(storedData);
+};
+// update isDone status from localstorage
+const updateData = (index) => {
+  let storedData = getPreviousData();
+  // Check if the index is valid
+  if (index >= 0 && index < storedData.length) {
+    // Use splice to remove the element at the specified index
+    let statusToUpdate = { ...storedData[index] };
+    statusToUpdate.isDone = true;
+    storedData[index] = statusToUpdate;
+    const updatedData = JSON.stringify(storedData);
+    localStorage.setItem('activitiesData', updatedData);
+    showData(storedData);
+  } else {
+    console.log('Invalid index');
+  }
+  // const updatedData = JSON.stringify(storedData);
+  // localStorage.setItem('activitiesData', updatedData);
+  // showData(storedData);
+};
+
 /**
  * Show data in UI
  */
@@ -56,21 +90,21 @@ const activitiesData = getPreviousData();
 const showData = (activities) => {
   const activitiesContainer = document.getElementById('all-activities');
   activitiesContainer.innerText = '';
-  activities?.forEach((data) => {
+  activities?.forEach((data, index) => {
     const activitiesWrapper = document.createElement('div');
 
     activitiesWrapper.innerHTML = `
     <div class="activities-record-wrapper">
             <div class="activities-record-container flex-between p-4">
               <div class="record-left-content">
-                <h2 class="fs-3">${data?.fullName}</h2>
+                <h2 class="fs-3">${data?.activityName}</h2>
                 <p><span>Aug 27, 2020</span> | <span>${
-                  data?.nickName
+                  data?.activityDetails
                 }</span></p>
               </div>
               <div class="record-right-content">
                   <div class='icon-flex check-icon rounded-circle ${
-                    data?.isDone === true ? 'd-block' : 'd-none'
+                    data?.isDone === true ? 'd-flex' : 'd-none'
                   }'>
                     <i class='fa-solid fa-check'></i>
                   </div>
@@ -86,9 +120,9 @@ const showData = (activities) => {
                   class="popup-buttons-card"
                   id="popupCard-${data?.id}"
                 >
-                  <p>Mark as Done</p>
+                  <p onclick="updateData(${index})">Mark as Done</p>
                   <hr class="horizontal-row" />
-                  <p>Delete</p>
+                  <p onclick="deleteData(${index})">Delete</p>
                 </div>
               </div>
             </div>
@@ -105,20 +139,20 @@ const handleFormSubmit = (event) => {
   event.preventDefault();
 
   const dateInput = document.getElementById('date');
-  const fullNameInput = document.getElementById('fullName');
-  const nickNameInput = document.getElementById('nickName');
+  const activityNameInput = document.getElementById('activityName');
+  const activityDetailsInput = document.getElementById('activityDetails');
 
   // store form data
   const formData = {
     id: getPreviousData()?.length + 1,
     date: dateInput.value,
-    fullName: fullNameInput.value,
-    nickName: nickNameInput.value,
+    activityName: activityNameInput.value,
+    activityDetails: activityDetailsInput.value,
     isDone: false,
   };
   addNewData(formData);
 
-  // reset form update data in ui & close modal
+  // reset form data, update in ui, & close modal
   event.target.reset();
   showData(getPreviousData());
   closeActivitiesModal();
